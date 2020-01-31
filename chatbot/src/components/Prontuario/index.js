1e282702f12a30dc33e965cb8cac701aa94499a1
Giancarlo <giandroberto@yahoo.com.br>
2020-01-31 10:15:53 -0300

    updates

--- chatbot/src/components/Prontuario/index.js
+++ chatbot/src/components/Prontuario/index.js
@@ -88,166 +88,168 @@ const Prontuario = () => {
 
   return (
     <div className="mainWrapper">
-      <div className="headerPart">
-        <div>
-          <h2 style={{ color: '#666', fontSize: '1.5rem' }}>
-  ELLEN&apos;S MEDICAL JOURNAL
-          </h2>
-        </div>
-        <div>
-          <button
-            type="button"
-            className={headerButton === '1' ? 'headerButton activeHeaderButton' : 'headerButton'}
-            onClick={() => setHeaderButton('1')}
-          >
-            BY DATE
-          </button>
-          <button
-            type="button"
-            className={headerButton === '2' ? 'headerButton activeHeaderButton' : 'headerButton'}
-            onClick={() => setHeaderButton('2')}
-          >
-            BY CONDITION
-          </button>
-          <button
-            type="button"
-            className={headerButton === '3' ? 'headerButton activeHeaderButton' : 'headerButton'}
-            onClick={() => setHeaderButton('3')}
-          >
-            BY TYPE
-          </button>
-        </div>
-      </div>
-      <div className="middlePart">
-        <div className="plusButton">
-          <button type="button" onClick={() => alert.show('Hello')}>+</button>
-        </div>
-        <div style={{ padding: 15, width: '80%', marginTop: 20 }}>
-          <Slider
-            defaultValue={20}
-            step={null}
-            marks={marks}
-            valueLabelDisplay="off"
-            value={value}
-            onChange={handleChange}
-          />
-        </div>
-      </div>
-      <div className="bottomPart">
-        <div className="bottomPart_left">
-          <button
-            className="bodyButton"
-            onClick={() => handleBodyFlex('1', 'flex-start')}
-            type="button"
-          >
-            <div className={bodyButton === '1' ? 'bodyButtonInner bodyButtonActive' : 'bodyButtonInner'} />
-          </button>
-          <button
-            className="bodyButton"
-            onClick={() => handleBodyFlex('2', 'center')}
-            type="button"
-          >
-            <div className={bodyButton === '2' ? 'bodyButtonInner bodyButtonActive' : 'bodyButtonInner'} />
-          </button>
-          <button
-            className="bodyButton"
-            onClick={() => handleBodyFlex('3', 'flex-end')}
-            type="button"
-          >
-            <div className={bodyButton === '3' ? 'bodyButtonInner bodyButtonActive' : 'bodyButtonInner'} />
-          </button>
-        </div>
-        <div className="bottomPart_right">
-          <div className="arrowContainer">
-            <div style={{
-              height: innerHeight - 50,
-              display: 'flex',
-              alignItems: flexMode,
-            }}
-            >
-              <div className="arrowContainerArrow" />
-            </div>
+      <div style={{flex: 1}}>
+        <div className="headerPart">
+          <div>
+            <h2 style={{ color: '#666', fontSize: '1.5rem' }}>
+    ELLEN&apos;S MEDICAL JOURNAL
+            </h2>
           </div>
-          <div className="info_box">
-            <div className="info_box_header">
-              <h2>Asthma</h2>
-              <div>
-                <p style={{ textAlign: 'right' }}>ACTIVE</p>
-                <p style={{ fontStyle: 'italic' }}>1947-present</p>
+          <div>
+            <button
+              type="button"
+              className={headerButton === '1' ? 'headerButton activeHeaderButton' : 'headerButton'}
+              onClick={() => setHeaderButton('1')}
+            >
+              BY DATE
+            </button>
+            <button
+              type="button"
+              className={headerButton === '2' ? 'headerButton activeHeaderButton' : 'headerButton'}
+              onClick={() => setHeaderButton('2')}
+            >
+              BY CONDITION
+            </button>
+            <button
+              type="button"
+              className={headerButton === '3' ? 'headerButton activeHeaderButton' : 'headerButton'}
+              onClick={() => setHeaderButton('3')}
+            >
+              BY TYPE
+            </button>
+          </div>
+        </div>
+        <div className="middlePart">
+          <div className="plusButton">
+            <button type="button" onClick={() => alert.show('Hello')}>+</button>
+          </div>
+          <div style={{ padding: 15, width: '80%', marginTop: 20 }}>
+            <Slider
+              defaultValue={20}
+              step={null}
+              marks={marks}
+              valueLabelDisplay="off"
+              value={value}
+              onChange={handleChange}
+            />
+          </div>
+        </div>
+        <div className="bottomPart">
+          <div className="bottomPart_left">
+            <button
+              className="bodyButton"
+              onClick={() => handleBodyFlex('1', 'flex-start')}
+              type="button"
+            >
+              <div className={bodyButton === '1' ? 'bodyButtonInner bodyButtonActive' : 'bodyButtonInner'} />
+            </button>
+            <button
+              className="bodyButton"
+              onClick={() => handleBodyFlex('2', 'center')}
+              type="button"
+            >
+              <div className={bodyButton === '2' ? 'bodyButtonInner bodyButtonActive' : 'bodyButtonInner'} />
+            </button>
+            <button
+              className="bodyButton"
+              onClick={() => handleBodyFlex('3', 'flex-end')}
+              type="button"
+            >
+              <div className={bodyButton === '3' ? 'bodyButtonInner bodyButtonActive' : 'bodyButtonInner'} />
+            </button>
+          </div>
+          <div className="bottomPart_right">
+            <div className="arrowContainer">
+              <div style={{
+                height: 512,
+                display: 'flex',
+                alignItems: flexMode,
+              }}
+              >
+                <div className="arrowContainerArrow" />
               </div>
             </div>
-            <div className="info_box_header_description">
-              <p>
-                Lorem ipsum dolor sit amet consectetur elit. Ut, ducimus?
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Repudiandae eum dolorum saepe eligendi natus
-                voluptate corporis molestias facere pariatur minima.
-              </p>
-            </div>
-            <div className="info_box_middle">
-              <div className="info_box_middle_info_up">
-                <div className="info_box_middle_medications">
-                  <h2>YOUR MEDICATIONS</h2>
-                  <div className="info_box_middle_medications_wrap">
-                    <div className="current">
-                      <p>CURRENT:</p>
-                    </div>
-                    <div className="symbicort">
-                      <h3>Symbicort</h3>
-                      <div style={{ margin: 12 }}>
-                        <p>One spray in each.</p>
-                        <p>Nostril as needed.</p>
+            <div className="info_box">
+              <div className="info_box_header">
+                <h2>Asthma</h2>
+                <div>
+                  <p style={{ textAlign: 'right' }}>ACTIVE</p>
+                  <p style={{ fontStyle: 'italic' }}>1947-present</p>
+                </div>
+              </div>
+              <div className="info_box_header_description">
+                <p>
+                  Lorem ipsum dolor sit amet consectetur elit. Ut, ducimus?
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                  Repudiandae eum dolorum saepe eligendi natus
+                  voluptate corporis molestias facere pariatur minima.
+                </p>
+              </div>
+              <div className="info_box_middle">
+                <div className="info_box_middle_info_up">
+                  <div className="info_box_middle_medications">
+                    <h2>YOUR MEDICATIONS</h2>
+                    <div className="info_box_middle_medications_wrap">
+                      <div className="current">
+                        <p>CURRENT:</p>
+                      </div>
+                      <div className="symbicort">
+                        <h3>Symbicort</h3>
+                        <div style={{ margin: 12 }}>
+                          <p>One spray in each.</p>
+                          <p>Nostril as needed.</p>
+                        </div>
                       </div>
                     </div>
                   </div>
+                  <div className="info_box_middle_medications" style={{ padding: 25, borderRight: 0 }}>
+                    <h2>YOUR SYMPTOMS</h2>
+                    <ul>
+                      <li>Weezing</li>
+                      <li>Shortness of breath</li>
+                    </ul>
+                  </div>
                 </div>
-                <div className="info_box_middle_medications" style={{ padding: 25, borderRight: 0 }}>
-                  <h2>YOUR SYMPTOMS</h2>
-                  <ul>
-                    <li>Weezing</li>
-                    <li>Shortness of breath</li>
-                  </ul>
+                <div className="info_box_middle_see_past">
+                  <div className="info_box_middle_see_past_button">
+                    <p>SEE PAST MEDICATIONS</p>
+                    <button
+                      type="button"
+                      onClick={() => alert.show('world')}
+                      style={{ cursor: 'pointer', border: 0, background: 'transparent' }}
+                    >
+                      <IoMdArrowDropdown />
+                    </button>
+                  </div>
                 </div>
               </div>
-              <div className="info_box_middle_see_past">
-                <div className="info_box_middle_see_past_button">
-                  <p>SEE PAST MEDICATIONS</p>
+              <div className="your_history">
+                <h2>YOUR HISTORY</h2>
+                <ChartsPage />
+              </div>
+              <div style={{ marginTop: 10, maxHeight: 200, overflowY: "scroll" }}>
+                {
+                  mockArr.slice(0, showItem).map(() => <Registro />)
+                }
+              </div>
+              {
+                (!endMock) && (
+                <div className="finalButton">
                   <button
                     type="button"
-                    onClick={() => alert.show('world')}
+                    onClick={loadMoreContent}
                     style={{ cursor: 'pointer', border: 0, background: 'transparent' }}
                   >
                     <IoMdArrowDropdown />
                   </button>
                 </div>
-              </div>
+                )
+  }
             </div>
-            <div className="your_history">
-              <h2>YOUR HISTORY</h2>
-              <ChartsPage />
-            </div>
-            <div style={{ marginTop: 10 }}>
-              {
-                mockArr.slice(0, showItem).map(() => <Registro />)
-              }
-            </div>
-            {
-              (!endMock) && (
-              <div className="finalButton">
-                <button
-                  type="button"
-                  onClick={loadMoreContent}
-                  style={{ cursor: 'pointer', border: 0, background: 'transparent' }}
-                >
-                  <IoMdArrowDropdown />
-                </button>
-              </div>
-              )
-}
           </div>
         </div>
+        <div />
       </div>
-      <div />
     </div>
   );
 };
